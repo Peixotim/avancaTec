bac1d94a62784a7b06df49ff64e0cb6d016ed246
Peixotim <pedropeixotovz@gmail.com>
2025-10-15 18:33:09 -0300

    Feat: Feat

--- public/SchemaOrg.tsx
+++ public/SchemaOrg.tsx
@@ -9,6 +9,10 @@ export default function SchemaOrg() {
     name: "Colégio AvançaTec",
     alternateName: [
       "AvançaTec",
+      "Colegio Avancatec",
+      "AvancaTec",
+      "colegio avancatec",
+      "avancatec",
       "Colégio Técnico AvançaTec",
       "Escola Técnica AvançaTec",
     ],
